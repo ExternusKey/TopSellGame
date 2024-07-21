aae6675f48a8574b1bc21c84eb70df15b1754567
ExternusKey <100219416+ExternusKey@users.noreply.github.com>
2024-07-21 15:16:46 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,77 +7,76 @@ class Program
     {
         string urlTopSellGame = "https://store.steampowered.com/search/?filter=topsellers&cc=ru";
         int countGames = 10;
+        string pageContent;
 
         try
         {
-            HttpClient httpClient = new();
-            using HttpClient client = httpClient;
-            HttpResponseMessage response = await client.GetAsync(urlTopSellGame);
-            response.EnsureSuccessStatusCode();
-            string pageContent = await response.Content.ReadAsStringAsync();
-
-            HtmlDocument doc = new();
-            doc.LoadHtml(pageContent);
-
-            var topSellNodes = doc.DocumentNode.SelectNodes("//div[@id='search_resultsRows']/a") ??
-                throw new ArgumentNullException("Не найдена таблица лидеров.");
-
-            var topSellGameList = topSellNodes
-                .Take(countGames)
-                .Select((node, index) => new
-                {
-                    rank = index + 1,
-                    gameName = node.SelectSingleNode(".//span[@class='title']")?.InnerText.Trim() ??
-                        throw new ArgumentNullException("Значение названия не может быть null."),
-
-                    gamePrice = node.SelectSingleNode(".//div[@class='discount_final_price']")?.InnerText.Trim() ??
-                        node.SelectSingleNode(".//div[@class='discount_final_price free']")?.InnerText.Trim().Replace("Free", "Бесплатно") ?? 
-                        throw new ArgumentNullException("Значение цены не может быть null."),
-
-                    gameUrl = node.GetAttributeValue("href", null) ??
-                        throw new ArgumentNullException("Значение ссылки не может быть null.")
-                    
-                });
-
-            int nonEmptyTopSellGameListCount = topSellGameList
-                .Where(node => !string.IsNullOrWhiteSpace(node.gameName))
-                .Count();
-
-            if (nonEmptyTopSellGameListCount < countGames)
-                Console.WriteLine($"Количество игр на странице {nonEmptyTopSellGameListCount}, что меньше заявленного количества " +
-                    $"({countGames}). Выводим Топ-{nonEmptyTopSellGameListCount}.");
-
-            while (true)
+            using (HttpClient client = new())
             {
-                Console.WriteLine($"Топ-{nonEmptyTopSellGameListCount} лидеров продаж в Steam (РФ):");
-                foreach (var gameInfo in topSellGameList)
-                    Console.WriteLine($"{gameInfo.rank,-2}. {gameInfo.gameName,-35} Стоимость - {gameInfo.gamePrice,12}");
-
-                Console.WriteLine("Если хотите перейти на страницу игры в браузере, введите номер позиции:");
-                Console.Write("Номер игры - ");
-                bool choose = int.TryParse(Console.ReadLine(), out int number);
-
-                if (number > 0 && number <= nonEmptyTopSellGameListCount)
-                {
-                    var game = topSellGameList.ElementAtOrDefault(number - 1);
-                    string link = game.gameUrl;
-
-                    Process.Start(new ProcessStartInfo
-                    {
-                        FileName = link,
-                        UseShellExecute = true
-                    });
-
-                    Console.Clear();
-                    Console.WriteLine($"Переход на страницу игры {game.gameName} успешно осуществлён.");
-                    break;
-                }
-                else
-                {
-                    Console.Clear();
-                    HandleError($"Некорретный ввод: Введите целое число [1 - {nonEmptyTopSellGameListCount}].");
-                }
+                HttpResponseMessage response = await client.GetAsync(urlTopSellGame);
+                response.EnsureSuccessStatusCode();
+                pageContent = await response.Content.ReadAsStringAsync();
             }
+			HtmlDocument doc = new();
+			doc.LoadHtml(pageContent);
+			
+			var topSellNodes = doc.DocumentNode.SelectNodes("//div[@id='search_resultsRows']/a") ??
+				throw new ArgumentNullException("Не найдена таблица лидеров.");
+
+			var topSellGameList = topSellNodes
+				.Take(countGames)
+				.Select((node, index) => new
+				{
+					rank = index + 1,
+					gameName = node.SelectSingleNode(".//span[@class='title']")?.InnerText.Trim() ??
+						throw new ArgumentNullException("Значение названия не может быть null."),
+
+					gamePrice = node.SelectSingleNode(".//div[@class='discount_final_price']")?.InnerText.Trim() ??
+						node.SelectSingleNode(".//div[@class='discount_final_price free']")?.InnerText.Trim().Replace("Free", "Бесплатно") ??
+						throw new ArgumentNullException("Значение цены не может быть null."),
+
+					gameUrl = node.GetAttributeValue("href", null) ??
+						throw new ArgumentNullException("Значение ссылки не может быть null.")
+
+				});
+
+			int nonEmptyTopSellGameListCount = topSellGameList
+				.Where(node => !string.IsNullOrWhiteSpace(node.gameName))
+				.Count();
+
+			if (nonEmptyTopSellGameListCount < countGames)
+				Console.WriteLine($"Количество игр на странице {nonEmptyTopSellGameListCount}, что меньше заявленного количества " +
+					$"({countGames}). Выводим Топ-{nonEmptyTopSellGameListCount}.");
+			
+			while (true)
+			{
+				Console.WriteLine($"Топ-{nonEmptyTopSellGameListCount} лидеров продаж в Steam (РФ):");
+				foreach (var gameInfo in topSellGameList)
+					Console.WriteLine($"{gameInfo.rank,-2}. {gameInfo.gameName,-35} Стоимость - {gameInfo.gamePrice,12}");
+
+				Console.WriteLine("Если хотите перейти на страницу игры в браузере, введите номер позиции:");
+				Console.Write("Номер игры - ");
+
+				if (int.TryParse(Console.ReadLine(), out int number) && number > 0 && number <= nonEmptyTopSellGameListCount)
+				{
+					var game = topSellGameList.ElementAt(number - 1);
+
+					Process.Start(new ProcessStartInfo
+					{
+						FileName = game.gameUrl,
+						UseShellExecute = true
+					});
+
+					Console.Clear();
+					Console.WriteLine($"Переход на страницу игры {game.gameName} успешно осуществлён.");
+					break;
+				}
+				else
+				{
+					Console.Clear();
+					HandleError($"Некорретный ввод: Введите целое число [1 - {nonEmptyTopSellGameListCount}].");
+				}
+			}            
         }
         catch (HttpRequestException e)
         {
